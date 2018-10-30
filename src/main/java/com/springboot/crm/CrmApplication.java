476cdaaacfac956688cd5f0974a3bed8cabb55cf
gjl <gjl.qq.com>
2018-10-30 09:21:55 +0800

    除去Shiro-框架搭建完成并测试跑通 为所有类声明作者

--- src/main/java/com/springboot/crm/CrmApplication.java
+++ src/main/java/com/springboot/crm/CrmApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Gjl
- * SringBootApplication 为springboot启动类注解，建议放在最外层
+ * @SringBootApplication 为springboot启动类注解，建议放在最外层
  * 其余controller以及包在此包下即可自动扫描 不用多余配置 不然扫描不到
- * ServletComponentScan为配置druid连接池时比加注释 而且只能在启动类加
- * ServletComponentScan为配置druid连接池时比加注释 而且只能在启动类加
+ * @ServletComponentScan为配置druid连接池时比加注释 而且只能在启动类加
  * 不可再自动以Controller加不然扫描不到
  */
 @SpringBootApplication
